46c1fda11e31964522daf78617494db6cbbe7d22
Shivangi <shivangimittal4196@gmail.com>
2020-10-03 16:02:04 +0530

    Minor Notes change

--- ComputerApp/ComputerApp.Android/obj/Release/android/environment.armeabi-v7a.s
+++ ComputerApp/ComputerApp.Android/obj/Release/android/environment.armeabi-v7a.s
@@ -23,8 +23,8 @@
 	.section	.rodata.env.str.1,"aMS",%progbits,1
 	.type	.L.env.str.1, %object
 .L.env.str.1:
-	.asciz	"com.companyname.computerapp"
-	.size	.L.env.str.1, 28
+	.asciz	"com.ShivangiMittal.computerapp.apk"
+	.size	.L.env.str.1, 35
 	.section	.data.application_config,"aw",%progbits
 	.type	application_config, %object
 	.p2align	2
@@ -49,7 +49,7 @@ application_config:
 	/* package_naming_policy */
 	.long	3
 	/* environment_variable_count */
-	.long	12
+	.long	10
 	/* system_property_count */
 	.long	0
 	/* android_package_name */
@@ -67,63 +67,53 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.3,"aMS",%progbits,1
 	.type	.L.env.str.3, %object
 .L.env.str.3:
-	.asciz	"MONO_DEBUG"
-	.size	.L.env.str.3, 11
+	.asciz	"MONO_GC_PARAMS"
+	.size	.L.env.str.3, 15
 	.section	.rodata.env.str.4,"aMS",%progbits,1
 	.type	.L.env.str.4, %object
 .L.env.str.4:
-	.asciz	"gen-compact-seq-points"
-	.size	.L.env.str.4, 23
+	.asciz	"major=marksweep-conc"
+	.size	.L.env.str.4, 21
 	.section	.rodata.env.str.5,"aMS",%progbits,1
 	.type	.L.env.str.5, %object
 .L.env.str.5:
-	.asciz	"MONO_GC_PARAMS"
-	.size	.L.env.str.5, 15
+	.asciz	"XAMARIN_BUILD_ID"
+	.size	.L.env.str.5, 17
 	.section	.rodata.env.str.6,"aMS",%progbits,1
 	.type	.L.env.str.6, %object
 .L.env.str.6:
-	.asciz	"major=marksweep-conc"
-	.size	.L.env.str.6, 21
+	.asciz	"411b47cc-002a-445e-b6a1-e79ffc6570da"
+	.size	.L.env.str.6, 37
 	.section	.rodata.env.str.7,"aMS",%progbits,1
 	.type	.L.env.str.7, %object
 .L.env.str.7:
-	.asciz	"XAMARIN_BUILD_ID"
-	.size	.L.env.str.7, 17
+	.asciz	"XA_HTTP_CLIENT_HANDLER_TYPE"
+	.size	.L.env.str.7, 28
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"77c326ca-97a1-42f3-b27b-53c1e5165ee6"
-	.size	.L.env.str.8, 37
+	.asciz	"Xamarin.Android.Net.AndroidClientHandler"
+	.size	.L.env.str.8, 41
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
 .L.env.str.9:
-	.asciz	"XA_HTTP_CLIENT_HANDLER_TYPE"
-	.size	.L.env.str.9, 28
+	.asciz	"XA_TLS_PROVIDER"
+	.size	.L.env.str.9, 16
 	.section	.rodata.env.str.10,"aMS",%progbits,1
 	.type	.L.env.str.10, %object
 .L.env.str.10:
-	.asciz	"Xamarin.Android.Net.AndroidClientHandler"
-	.size	.L.env.str.10, 41
+	.asciz	"btls"
+	.size	.L.env.str.10, 5
 	.section	.rodata.env.str.11,"aMS",%progbits,1
 	.type	.L.env.str.11, %object
 .L.env.str.11:
-	.asciz	"XA_TLS_PROVIDER"
-	.size	.L.env.str.11, 16
+	.asciz	"__XA_PACKAGE_NAMING_POLICY__"
+	.size	.L.env.str.11, 29
 	.section	.rodata.env.str.12,"aMS",%progbits,1
 	.type	.L.env.str.12, %object
 .L.env.str.12:
-	.asciz	"btls"
-	.size	.L.env.str.12, 5
-	.section	.rodata.env.str.13,"aMS",%progbits,1
-	.type	.L.env.str.13, %object
-.L.env.str.13:
-	.asciz	"__XA_PACKAGE_NAMING_POLICY__"
-	.size	.L.env.str.13, 29
-	.section	.rodata.env.str.14,"aMS",%progbits,1
-	.type	.L.env.str.14, %object
-.L.env.str.14:
 	.asciz	"LowercaseCrc64"
-	.size	.L.env.str.14, 15
+	.size	.L.env.str.12, 15
 	.section	.data.app_environment_variables,"aw",%progbits
 	.type	app_environment_variables, %object
 	.p2align	2
@@ -139,9 +129,7 @@ app_environment_variables:
 	.long	.L.env.str.10
 	.long	.L.env.str.11
 	.long	.L.env.str.12
-	.long	.L.env.str.13
-	.long	.L.env.str.14
-	.size	app_environment_variables, 48
+	.size	app_environment_variables, 40
 	.section	.data.app_system_properties,"aw",%progbits
 	.type	app_system_properties, %object
 	.p2align	2
